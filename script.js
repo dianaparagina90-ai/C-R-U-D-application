770537fe57200ebbd2d2c2b8ba745cf2b94439f0
dianaparagina90-ai <diana.paragina90@gmail.com>
2026-03-16 18:39:24 +0100

    add picture into db files and function to paint all games on html

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+const allGames = document.querySelector(".all-games");
 
 async function getData() {
     const data = await fetch("http://localhost:3000/games")
-    const databas = await data.json()
-    return databas 
+    const games = await data.json()
+
+    games.forEach(game => {
+        const card = document.createElement("div")
+        card.classList.add("games")
+        const title = document.createElement("h3")
+        title.textContent = game.title
+        const img = document.createElement("img")
+        img.src = game.image
+
+        const button = document.createElement("button")
+        button.textContent = "Läs mer"
+
+        card.appendChild(title)
+        card.appendChild(img)
+        card.appendChild(button)
+        allGames.appendChild(card)
+        
+    });
+
+    
+      
 }
 
+getData()
