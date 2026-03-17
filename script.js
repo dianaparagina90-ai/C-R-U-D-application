d9a7e69e9c926bc32273bbdc15bcba37c6e777b5
dianaparagina90-ai <diana.paragina90@gmail.com>
2026-03-17 13:24:27 +0100

    add background-image and try-catch

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const allGames = document.querySelector(".all-games");
 
 async function getData() {
-    const data = await fetch("http://localhost:3000/games")
-    const games = await data.json()
 
-    games.forEach(game => {
+    try {
+        const data = await fetch("http://localhost:3000/games")
+
+        if(!data.ok) {
+            throw new Error("Kunde inte hämta spel från servern.")
+        }
+
+        const games = await data.json()
+
+        games.forEach(game => {
         const card = document.createElement("div")
         card.classList.add("games")
         const title = document.createElement("h3")
@@ -19,11 +26,18 @@ async function getData() {
         card.appendChild(img)
         card.appendChild(button)
         allGames.appendChild(card)
-        
-    });
 
-    
-      
+        });
+        
+    } catch (error) {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "⚠️ Något gick fel när spelen skulle laddas. Försök igen senare.";
+        errorMessage.style.color = "red";
+
+        allGames.appendChild(errorMessage);
+
+        console.error(error);
+    } 
 }
 
 getData()
